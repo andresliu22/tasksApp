4f4e1c2f2ba5f2465601ad3f7bceee7c2bb6bc67
Andres Liu <andres.liu22@gmail.com>
2021-09-20 20:30:52 -0700

    finished project

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Andres Liu on 8/4/21.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+//        let newCount = count - 1
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+    }
+}
